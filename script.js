fef67a905846f71486772e6b545fdef3f67beb0e
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-02 19:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const blockInput=document.querrySelector("#block_id");
+const colorInput=document.querrySelector("#color_id");
+const  colorbtn=document.querrySelector("#change_button");
+const resetbtn=document.querrySelector("#Reset");
