5beab75f67e0bb666eee1ed3b78011f19eafbfd1
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-02 20:13:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 //your JS code here. If required.
 const blockInput=document.querrySelector("#block_id");
 const colorInput=document.querrySelector("#color_id");
-const  colorbtn=document.querrySelector("#change_button");
-const resetbtn=document.querrySelector("#Reset");
+const  colorbtn=document.querySelector("#change_button");
+const resetbtn=document.querySelector("#Reset");
+const boxs=document.querySelectorAll(".colorBox");
+
+colorbtn.addEventListener("click",()=>{
+	const blockValue=blockInput.value;
+	const colorValue=colorInput.value;
+	for(let i=0;i<boxs.length;i++){
+		if(i+1===blockValue){
+			boxs[i+1].style.backgroundColor=`${colorValue}`
+		}
+	}
+})
